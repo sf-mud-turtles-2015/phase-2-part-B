9af0897631ce8914a1fccf3bd026893bd2efddb1
Julian Bouma <julian@devbootcamp.com>
2015-10-13 11:25:21 -0700

    basic structure

---
+++ part-1/classroom.js
@@ -0,0 +1,11 @@
+var Classroom = function(students) {
+  this.students = students;
+}
+
+Classroom.prototype.find = function(name) {
+
+}
+
+Classroom.prototype.honorRollStudents() = function() {
+
+}
---
+++ part-1/student.js
@@ -0,0 +1,12 @@
+var Student = function(firstName, scores) {
+  this.firstName = firstName;
+  this.scores = scores;
+}
+
+Student.prototype.averageScore = function() {
+
+}
+
+Student.prototype.letterGrade = function() {
+
+}
