4fd32ed61aced9ff155333ff0ddbf93f2bef1d86
Si Cheng Zhou <email": null>
2015-10-13 09:54:23 -0700

    refactor

--- part-1/classroom.js
+++ part-1/classroom.js
@@ -4,18 +4,14 @@ var Classroom = function(students){
   this.find = function(name){
     var targetStudent //tricky!
     students.forEach(function(student){
-      if (student.firstName === name){
-        targetStudent = student //tricky! I wanted to return student here but it returns undefined!
-      }
+      if (student.firstName === name) targetStudent = student //tricky! I wanted to return student here but it returns undefined!
     })
     return targetStudent //tricky!
   }
   this.honorRollStudents = function(){
     var honorStudents = []
     students.forEach(function(student){
-      if (student.averageScore() >= 95){
-        honorStudents.push(student)
-      }
+      if (student.averageScore() >= 95) honorStudents.push(student)
     })
     return honorStudents
   }
