b05dfde3a6c40cab9ab3db69730dd20f27d4350a
Drew Peterson <drew.m.peterson@gmail.com>
2015-10-13 12:27:44 -0700

    Add additional comments and refactoring...

--- part-2/public/js/application.js
+++ part-2/public/js/application.js
@@ -3,27 +3,32 @@ $(document).ready(function(){
   //click even on share your thoughts
   $("#new_post_link").on("click", function(e){
     e.preventDefault();
-    // $(this).hide()
+    // $(this).hide() // hide the link
     var url = $(this).attr("href")
     $.get(url ,function(data){
       $("#sidebar").append(data)
     })
 
     // never done a click event inside a click event...
+    // alternative way is to bring the html form in the view on this page
+    // then all you did is hide and show, this will be faster...
+
     $("#sidebar").on("submit", "#post_form", function(e){
       e.preventDefault()
 
-      var url = $("#post_form").attr("action")
-      //need to double check why the grabbing the form doesnt work here...
+      // this is referring to the #post_form because I am using even delegation..
+      // I dont think i need event delegation but yeah here is an example....
+      console.log($(this))
+
+      var url = $(this).attr("action")
       var data = $(this).serialize()
-      // console.log(url)
-      // console.log(data)
 
       $.post(url, data, function(response){
-        // console.log(response)
+        //response will be the partial
         $("#posts").append(response)
         $("#post_form").remove()
 
+        //$(this).show() //show the link later on...
       })
     })
   })
