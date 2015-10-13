42abd397f0a144b07add0b27a4537777d7212193
Julian Bouma <julian@devbootcamp.com>
2015-10-13 11:30:15 -0700

    student first draft

--- part-1/student.js
+++ part-1/student.js
@@ -4,9 +4,21 @@ var Student = function(firstName, scores) {
 }
 
 Student.prototype.averageScore = function() {
+  var total = this.scores.reduce(function(x, y) { return x + y });
+  return total / this.scores.length;
 
+  // var total = 0;
+  // for (var i = 0; i < this.scores.length; i++) {
+  //   total += this.scores[i];
+  // }
+  // return total;
 }
 
 Student.prototype.letterGrade = function() {
-
+  var score = this.averageScore();
+  if (score >= 90) { return "A" };
+  if (score >= 80) { return "B" };
+  if (score >= 70) { return "C" };
+  if (score >= 60) { return "D" };
+  return "F";
 }
