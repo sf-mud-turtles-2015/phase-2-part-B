817399ccb7d7f6ae8d0049f7210c52c1baee6d10
Si Cheng Zhou <email": null>
2015-10-13 09:52:40 -0700

    finish challenge

--- part-1/classroom.js
+++ part-1/classroom.js
@@ -1,3 +1,22 @@
-var Classroom = function(){
-
+var Classroom = function(students){
+  this.students = students;
+  console.log(this.students)
+  this.find = function(name){
+    var targetStudent //tricky!
+    students.forEach(function(student){
+      if (student.firstName === name){
+        targetStudent = student //tricky! I wanted to return student here but it returns undefined!
+      }
+    })
+    return targetStudent //tricky!
+  }
+  this.honorRollStudents = function(){
+    var honorStudents = []
+    students.forEach(function(student){
+      if (student.averageScore() >= 95){
+        honorStudents.push(student)
+      }
+    })
+    return honorStudents
+  }
 }
