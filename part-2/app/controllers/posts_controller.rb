045aef9cfd36946728e0b3c4486a015af9c41e8e
Masha Lifshin <mlifshin@gmail.com>
2015-10-14 17:44:26 -0700

    Ajax notes

--- part-2/app/controllers/posts_controller.rb
+++ part-2/app/controllers/posts_controller.rb
@@ -6,25 +6,39 @@ end
 post "/posts" do
   @post = Post.new(params[:post])
 
-  if @post.save
-    if request.xhr?
-      content_type :json
-      @post.to_json
-    else
-      redirect "posts/#{@post.id}"
-    end
+  if request.xhr?
+    # content_type :json
+    # @post.to_json
+    # erb aka render the post partial with layout false and the locals
   else
-    if request.xhr?
-      p "didn't save! and this is an XHR request"
-    else
-      p "didn't save! and this is a normal request"
-    end
+    redirect "posts/#{@post.id}"
   end
+
+  # if @post.save
+  #   if request.xhr?
+  #     content_type :json
+  #     @post.to_json
+  #   else
+  #     redirect "posts/#{@post.id}"
+  #   end
+  # else
+  #   if request.xhr?
+  #     p "didn't save! and this is an XHR request"
+  #   else
+  #     p "didn't save! and this is a normal request"
+  #   end
+  # end
 end
 
 get "/posts/new" do
   @post = Post.new
-  erb :'posts/new'
+  if request.xhr?
+    # new behavior to erb just the form partial
+    # layout is false
+    # pass 1 local variable to the partial, which expects a variable called post
+  else
+    erb :'posts/new'
+  end
 end
 
 get "/posts/:id" do
--- part-2/public/js/application.js
+++ part-2/public/js/application.js
@@ -2,25 +2,29 @@ $( document ).ready(function() {
   $('.button_anchor').on('click', function(event) {
     event.preventDefault();
     $(this).css("display", "none")
-        console.log( "I got clicked!" );
-      $.ajax ({
-        url: '/posts/new',
-        type: 'GET'
-      }).done(function(data) {
-        $('.float_wrapper').append($(data).find('#post_form_container').css "display", "inline")
-      });
-  })
-
-
-$('#post_form_container').on('submit', function(event) {
-    event.preventDefault();
-      $.ajax ({
-        url: '/posts',
-        type: 'post'
-      }).done(function(data) {
-        $('.float_wrapper').append(data)
-      });
+    console.log( "I got clicked!" );
+    $.ajax ({
+      url: '/posts/new',
+      type: 'GET'
+    }).done(function(response) {
+      // $('.float_wrapper').append($(data).find('#post_form_container').css("display", "inline")
+    });
   });
-    $('#post_form_container').css("display", "none")
-    $('#sidebar').css("display", "inline")
+
+  $('#post_form').on('submit', function(event) {
+    event.preventDefault();
+    var data = $(this).serialize();
+    $.ajax ({
+      url: $(this).attr('action'),
+      type: 'post',
+      data: data
+    }).done(function(response) {
+      $('.float_wrapper').append(response)
+      // $('#the_list_of_posts').append(response)
+
+    });
+  });
+
+  //$('#post_form_container').css("display", "none")
+  //$('#sidebar').css("display", "inline")
 });
